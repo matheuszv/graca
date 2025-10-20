81413764da6003d91e27de4cc5aed3c0758e2ab4
SpaidiiPlayer <matheus.vinagre2@gmail.com>
2025-10-19 22:13:26 -0300

    versao 1.0

--- src/app/encontrepontos/page.tsx
+++ src/app/encontrepontos/page.tsx
@@ -1,6 +1,7 @@
 import { getCidades } from "@/lib/getCidades"
 import { PontosPublic } from "@/components/pontosSemLogin"
 
+export const dynamic = 'force-dynamic'
 export default async function Page() {
   const { cidades, apoioLista } = await getCidades()
 
