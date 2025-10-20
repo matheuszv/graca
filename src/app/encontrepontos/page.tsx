deb12e94216a3ba5c2afab1ed732b991fd1e0c68
SpaidiiPlayer <matheus.vinagre2@gmail.com>
2025-10-19 21:35:03 -0300

    teste

--- src/app/encontrepontos/page.tsx
+++ src/app/encontrepontos/page.tsx
@@ -1,26 +1,12 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-'use server'
-import prisma from "@/lib/prisma"
+import { getCidades } from "@/lib/getCidades"
 import { PontosPublic } from "@/components/pontosSemLogin"
 
-export default async function getCidades() {
-    const result = await prisma.cidades.findMany()
+export default async function Page() {
+  const { cidades, apoioLista } = await getCidades()
 
-    const cidadesFormatadas = result.map((cidade: any) => {
-        return {
-          id: cidade.codigo_ibge,
-          nome: cidade.nome_completo,
-          coord: [cidade.coord[1], cidade.coord[0]] as [number, number]
-        }
-    })
-
-    const pontos = await prisma.pontos.findMany()
-
-    const apoioLista = pontos.map((apoio: any)=>{
-        return {...apoio, coordenada: apoio.coordenada.split(",") as [number, number], data: apoio.data.split(",")}
-    })
-
-    return (
-        <PontosPublic apoioLista={apoioLista} cidades={cidadesFormatadas}/>
-    )
+  return (
+    <div>
+      <PontosPublic cidades={cidades} apoioLista={apoioLista} />
+    </div>
+  )
 }
---
+++ src/lib/getCidades.ts
@@ -0,0 +1,25 @@
+import prisma from "@/lib/prisma"
+
+export async function getCidades() {
+  const result = await prisma.cidades.findMany()
+
+  const cidadesFormatadas = result.map((cidade: any) => {
+    return {
+      id: cidade.codigo_ibge,
+      nome: cidade.nome_completo,
+      coord: [cidade.coord[1], cidade.coord[0]] as [number, number],
+    }
+  })
+
+  const pontos = await prisma.pontos.findMany()
+
+  const apoioLista = pontos.map((apoio: any) => {
+    return {
+      ...apoio,
+      coordenada: apoio.coordenada.split(",") as [number, number],
+      data: apoio.data.split(","),
+    }
+  })
+
+  return { cidades: cidadesFormatadas, apoioLista }
+}
